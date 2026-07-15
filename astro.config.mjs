7fbd3ec6f241da073a8e9dee27ebcda613add193
Jheison-Flores <floresdeiner2003@gmail.com>
2026-07-15 16:49:13 -0500

    Corregir ruta de imagen de aliados

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://teincomin.netlify.app', // <--- ¡Añade esto!
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  integrations: [sitemap()]
 });
